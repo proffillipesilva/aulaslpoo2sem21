0b3f43860175a03ae6ba0b42308bb8e705ea4c5f
zPauloH <89671244+zPauloH@users.noreply.github.com>
2021-09-02 22:12:19 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,20 @@
+def main():
+	nota1 = int (input())
+	nota2 = int (input())
+	
+	soma = nota1 + nota2
+	media = nota1 + nota2 / 2
+	
+	if media >= 5 :
+		print ("passou")
+	else :
+		print ("bombou")
+	
+main()
+
+
+
+
+
+
+	
