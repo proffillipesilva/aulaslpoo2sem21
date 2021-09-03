473bee817a1af72c467ff09b0150498bb1c7147c
melfernanda <89671448+melfernanda@users.noreply.github.com>
2021-09-02 22:32:58 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,13 @@
+def main():
+	nota1= int(input("nota1"))
+	nota2= int(input("nota2"))
+	
+	soma= nota1 + nota2
+	media= soma/2
+
+	if media >= 5:
+	print('passou')
+	else:
+	print('bombou')
+
+main()
