5f35c48c4901cd4edf02185cd7ac22a07d7a8ca1
Pietro Juan <pjuanfreire@gmail.com>
2021-09-02 22:19:07 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,12 @@
+def main():
+	#
+	nota1 = int(input("Digite a nota 1:"))
+	nota2 = int(input("Digite a nota 2:"))
+	#
+	if (nota1 + nota2)/2 >= 5:
+		print("Passou")
+	#
+	else:
+		print("Bombou")
+
+main()
