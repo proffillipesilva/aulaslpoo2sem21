426126589ae147b53e7b48ddec64b75e34b10165
MatheusSSHub <89671426+MatheusSSHub@users.noreply.github.com>
2021-09-02 22:28:42 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,13 @@
+def main()
+    nota_1 = int(2)
+    nota_2 = int(2)
+    
+    soma = nota_1 + nota_2
+    media = soma/2
+    
+    if media >= 5:
+        print('passou')
+    else:
+        print('bombou')
+        
+main()
