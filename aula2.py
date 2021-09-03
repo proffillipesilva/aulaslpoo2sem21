a35ebbbe368066d1393fce3b0c1993dcbd0a2cc9
Juliane Bueno Francisco <89671282+kymberllyth@users.noreply.github.com>
2021-09-02 22:09:40 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,10 @@
+def main ():
+	#
+	nota1 = float (input("insira nota1:"))
+	nota2 = float (input("insira nota2:"))
+	media = (nota1+nota2)/2
+
+	if media >= 5: print ("Passou")
+	else: print ("Bombou")
+	#
+main()
